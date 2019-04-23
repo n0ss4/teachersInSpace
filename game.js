d6406d5878b28de41f8204348b594faee819b18e
Nossair <n0ss4@protonmail.com>
2019-04-23 16:31:57 +0200

    cambios

--- game.js
+++ game.js
@@ -19,30 +19,34 @@ var profesores = [];
 var elegido;
 
 // TAMAÑO del VIDEOJUEGO [RESPONSIVE]
-var w = window.outerWidth-30;
-var h = window.outerHeight-110;
+var w = window.outerWidth - 30;
+var h = window.outerHeight - 110;
 
 // EL PUNTO "Y" DE LAS IMAGENES.
-var y = h-350;
+var y = h - 350;
+
+//
+var bullets;
+var ship;
+var speed;
+var stats;
+var cursors;
+var lastFired = 0;
 
 var config = {
-    type: Phaser.WEBGL,
-    width: w,
-    height: h,
-    backgroundColor: "black",
-    physics: {
-      default: "arcade",
-      arcade: {
-        gravity: { x: 0, y: 0 }
-      }
-    },
-    scene: [
-        Inicio,
-        EscogerJugador,
-        PantallaJuego
-    ],
-    pixelArt: true,
-    roundPixels: true
-  };
-  
-  var game = new Phaser.Game(config);
+  type: Phaser.WEBGL,
+  width: w,
+  height: h,
+  backgroundColor: "black",
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { x: 0, y: 0 }
+    }
+  },
+  scene: [Inicio, EscogerJugador, PantallaJuego],
+  pixelArt: true,
+  roundPixels: true
+};
+
+var game = new Phaser.Game(config);
--- pantallaJuego.js
+++ pantallaJuego.js
@@ -10,6 +10,7 @@ class PantallaJuego extends Phaser.Scene {
   preload() {
     // FONDO
     this.load.image("fondo", "img/starfield.png");
+    this.load.image("bullet", "img/bullet.png");
   }
 
   create() {
@@ -17,27 +18,59 @@ class PantallaJuego extends Phaser.Scene {
     this.bg = this.add
       .tileSprite(0, 0, game.config.width, game.config.height, "fondo")
       .setOrigin(0);
+    var Bullet = new Phaser.Class({
+      Extends: Phaser.GameObjects.Image,
 
-    //
-    this.x = this.add.sprite(w / 2, h - 100, this.jugador);
+      initialize: function Bullet(scene) {
+        Phaser.GameObjects.Image.call(this, scene, 0, 0, "bullet");
 
-    // Para que se puedan mover los INPUT's.
-    this.keyLeft = this.input.keyboard.addKey(
-      Phaser.Input.Keyboard.KeyCodes.LEFT
-    );
-    this.keyRight = this.input.keyboard.addKey(
-      Phaser.Input.Keyboard.KeyCodes.RIGHT
-    );
+        this.speed = Phaser.Math.GetSpeed(w / 2, 1);
+      },
+
+      fire: function(x, y) {
+        this.setPosition(x, y - 50);
+
+        this.setActive(true);
+        this.setVisible(true);
+      },
+
+      update: function(time, delta) {
+        this.y -= this.speed * delta;
+
+        if (this.y < -50) {
+          this.setActive(false);
+          this.setVisible(false);
+        }
+      }
+    });
+
+    bullets = this.add.group({
+      classType: Bullet,
+      maxSize: 10,
+      runChildUpdate: true
+    });
+
+    ship = this.add.sprite(w / 2, h - 100, this.jugador).setDepth(1);
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    speed = Phaser.Math.GetSpeed(300, 1);
   }
 
-  update() {
-    if (this.x != undefined) {
-      if (this.keyLeft.isDown) {
-        this.x.x = this.x.x - 10;
-      }
+  update(time, delta) {
+    if (cursors.left.isDown) {
+      ship.x -= speed * delta;
+    } else if (cursors.right.isDown) {
+      ship.x += speed * delta;
+    }
 
-      if (this.keyRight.isDown) {
-        this.x.x = this.x.x + 10;
+    if (cursors.up.isDown && time > lastFired) {
+      var bullet = bullets.get();
+
+      if (bullet) {
+        bullet.fire(ship.x, ship.y);
+
+        lastFired = time + 50;
       }
     }
   }
