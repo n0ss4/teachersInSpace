dbbf7f847370d15ce422152ace5d6daba043352e
Nossair <n0ss4@protonmail.com>
2019-04-23 15:19:40 +0200

    Pasar el jugador seleccionado por la scene.

--- pantallaJuego.js
+++ pantallaJuego.js
@@ -1,42 +1,44 @@
 class PantallaJuego extends Phaser.Scene {
-    constructor(){
-        super({ key: "PantallaJuego" });
+  constructor() {
+    super({ key: "PantallaJuego" });
+  }
+
+  init(data) {
+    this.jugador = data.texture.key;
+  }
+
+  preload() {
+    // FONDO
+    this.load.image("fondo", "img/starfield.png");
+  }
+
+  create() {
+    // IMAGEN de FONDO a 100%
+    this.bg = this.add
+      .tileSprite(0, 0, game.config.width, game.config.height, "fondo")
+      .setOrigin(0);
+
+    //
+    this.x = this.add.sprite(w / 2, h - 100, this.jugador);
+
+    // Para que se puedan mover los INPUT's.
+    this.keyLeft = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.LEFT
+    );
+    this.keyRight = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.RIGHT
+    );
+  }
+
+  update() {
+    if (this.x != undefined) {
+      if (this.keyLeft.isDown) {
+        this.x.x = this.x.x - 10;
+      }
+
+      if (this.keyRight.isDown) {
+        this.x.x = this.x.x + 10;
+      }
     }
-
-    init (data)
-    {
-        this.jugador = data;
-    }
-
-    preload(){
-        // FONDO
-        this.load.image('fondo', 'img/starfield.png');
-        
-    }
-
-
-    create(){
-        // IMAGEN de FONDO a 100%
-        this.bg = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'fondo').setOrigin(0);
-
-        //
-        this.x = this.add.sprite(w/2,h-100,elegido);
-
-
-        // Para que se puedan mover los INPUT's.
-        this.keyLeft = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-        this.keyRight = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-    }
-
-    update(){
-        if (this.x != undefined) {
-            if (this.keyLeft.isDown) {
-                this.x.x = this.x.x - 10;
-            }
-        
-            if(this.keyRight.isDown){
-                this.x.x = this.x.x + 10;
-            }
-        }
-    }
+  }
 }
